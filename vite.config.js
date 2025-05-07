5012b6230f1199cdd09f4421eab25f1a7db2a8b7
dwerkjem <derekrneilson@gmail.com>
2025-05-06 20:31:23 -0600

    fix: remove unnecessary comment from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-// filepath: /home/derek/DEV/Hebrew-Transliteration-Trainer/vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // replace with your GitHub username and repo name
   base: '/Hebrew-Transliteration-Trainer/'
 })
